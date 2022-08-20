b20ad2f2166f1b67b7b3cbdeb72dcf0645c32034
akshaygupta <akgupta@falabella.cl>
2022-08-20 23:43:01 +0530

    [ProjectSetup-Hackathon | Akshay | RewardManagement]

---
+++ src/main/java/com/hackathon/RewardManagement/util/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.hackathon.RewardManagement.util;
+
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+public class ApplicationConstants {
+
+    public static final String EVENT_ORDER_CREATED = "ORDERCREATED";
+}
